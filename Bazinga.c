08add675df352573fdd1f74689ea1468b9ea5bbf
keshrisaksham <keshrisaksham@outlook.com>
2025-12-05 13:27:57 +0530

    Create Bazinga.c

---
+++ Bazinga.c
@@ -0,0 +1,16 @@
+#include <windows.h>
+
+// Standard DLL Entry Point
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
+    switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH:
+        // This code runs immediately when the DLL is loaded
+        WinExec("calc.exe", SW_SHOW);
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
